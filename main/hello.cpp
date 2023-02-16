7bbb2e5935bf2cfd7a3eccca831cce874220bbeb
ShubangiSaxena <74584556+Shubangi-Saxena@users.noreply.github.com>
2023-02-16 12:05:10 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout<< "PES1UG20CS418\n";
+  cout<< "Shubangi Saxena\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
