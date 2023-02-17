3a663220c2457e1b0a10b9a5a9be2366c7ca4957
ShubangiSaxena <74584556+Shubangi-Saxena@users.noreply.github.com>
2023-02-17 14:47:42 +0530

    Update jenkins_PES1UG20CS418.cpp

--- jenkins_PES1UG20CS418.cpp
+++ jenkins_PES1UG20CS418.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(void) {
-  cout<< "PES1UG20CS418_ShubangiSaxena\n";
+  cout<< "PES1UG20CS418_ShubangiSaxena\n"
   cout << "Creating my own pipeline and printing SRN and name";
   return 0;
 }
